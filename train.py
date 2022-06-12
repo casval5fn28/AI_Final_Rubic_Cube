0e6dc0174ee8f4cd67c776710b3f4244b3511d36
Casval_dreamwalker <71252273+casval5fn28@users.noreply.github.com>
2022-06-13 02:02:26 +0800

    Update train.py

--- train.py
+++ train.py
@@ -48,21 +48,14 @@ A simple class to manage configuration
 """
 
 class Config():
-    #training_dir = "./data/cards_old/training/"
-    #testing_dir = "./data/cards_old/testing/"
-    
-    #training_dir = "./data/cards/training/"
-    #testing_dir = "./data/cards/testing/"
-    
-    #training_dir = "./cardDatabase/"
-    #testing_dir = "./cardDatabase/"
+
      
     training_dir = "./cardDatabaseFull/"
     testing_dir = "./cardDatabaseFull/"
+    #training_dir = "https://db.ygoprodeck.com/api/v7/cardinfo.php"
+    #testing_dir = "https://db.ygoprodeck.com/api/v7/cardinfo.php"
     
-    #testing_dir = "./data/cards_old/testing/"
-    
-    #train_batch_size = 24
+
     train_batch_size = 24*2
 
     #train_batch_size = 8
@@ -161,182 +154,157 @@ class ImgAugTransform:
         img = np.array(img)
         return self.aug.augment_image(img)
 
-# https://colab.research.google.com/drive/109vu3F1LTzD1gdVV6cho9fKGx7lzbFll#scrollTo=aUpukiy8sBKx
-siamese_dataset = SiameseNetworkDataset(imageFolderDataset=folder_dataset,
-                                        transform=transforms.Compose([
-                                                    transforms.Grayscale(num_output_channels=3),
-                                                    #transforms.Resize((100,100)),
-                                                    transforms.Resize((244,244)),
-                                                    transforms.ColorJitter(brightness=(0.5,1.5),contrast=(0.3,2.0),hue=.05, saturation=(.0,.15)),
-                                                    #transforms.RandomHorizontalFlip(),
-                                                    #transforms.RandomRotation(10),
-                                                    
-                                                    transforms.RandomAffine(0, translate=(0,0.3), scale=(0.6,1.8), shear=(0.0,0.4), resample=False, fillcolor=0),
-                                                    transforms.ToTensor(),
-                                                    transforms.Normalize([0.485, 0.456, 0.406],
-                                                                                                                                                            [0.229, 0.224, 0.225]),
-                                                ])
-                                       ,should_invert=False)
+if __name__=='__main__' :
+    # https://colab.research.google.com/drive/109vu3F1LTzD1gdVV6cho9fKGx7lzbFll#scrollTo=aUpukiy8sBKx
+    siamese_dataset = SiameseNetworkDataset(imageFolderDataset=folder_dataset,
+                                            transform=transforms.Compose([
+                                                        transforms.Grayscale(num_output_channels=3),
+                                                        #transforms.Resize((100,100)),
+                                                        transforms.Resize((244,244)),
+                                                        transforms.ColorJitter(brightness=(0.5,1.5),contrast=(0.3,2.0),hue=.05, saturation=(.0,.15)),
+                                                        #transforms.RandomHorizontalFlip(),
+                                                        #transforms.RandomRotation(10),
+                                                        
+                                                        transforms.RandomAffine(0, translate=(0,0.3), scale=(0.6,1.8), shear=(0.0,0.4), resample=False, fillcolor=0),
+                                                        transforms.ToTensor(),
+                                                        transforms.Normalize([0.485, 0.456, 0.406],
+                                                                                                                                                                [0.229, 0.224, 0.225]),
+                                                    ])
+                                           ,should_invert=False)
 
 
 
-vis_dataloader = DataLoader(siamese_dataset,
-                        shuffle=True,
-                        num_workers=8,
-                        #num_workers=0,
-                        batch_size=8)
-dataiter = iter(vis_dataloader)
+    vis_dataloader = DataLoader(siamese_dataset,
+                            shuffle=True,
+                            num_workers=8,
+                            #num_workers=0,
+                            batch_size=8)
+    dataiter = iter(vis_dataloader)
 
 
-        
-class SiameseNetwork(nn.Module):
-    def __init__(self):
-        super(SiameseNetwork, self).__init__()
+            
+    class SiameseNetwork(nn.Module):
+        def __init__(self):
+            super(SiameseNetwork, self).__init__()
 
 
-        #self.resnet = models.resnet152(pretrained=True)
-        self.resnet = models.resnet101(pretrained=True)
-        #self.resnet = models.resnet50(pretrained=True)
+            #self.resnet = models.resnet152(pretrained=True)
+            self.resnet = models.resnet101(pretrained=True)
+            #self.resnet = models.resnet50(pretrained=True)
 
-        #self.resnet = torch.nn.Sequential(*(list(self.resnet.children())[:-1]))
+            #self.resnet = torch.nn.Sequential(*(list(self.resnet.children())[:-1]))
 
-    def forward_once(self, x):
-        '''
-        output = self.cnn1(x)
-        output = output.view(output.size()[0], -1)
-        output = self.fc1(output)
-        #print(output.shape)
-        #print(output)
-        '''
-        #begin = time()
-        output = self.resnet(x)
-        #print('Time for forward prop: ', time()-begin)
+        def forward_once(self, x):
+            '''
+            output = self.cnn1(x)
+            output = output.view(output.size()[0], -1)
+            output = self.fc1(output)
+            #print(output.shape)
+            #print(output)
+            '''
+            #begin = time()
+            output = self.resnet(x)
+            #print('Time for forward prop: ', time()-begin)
 
-        return output
-        
+            return output
+            
 
-    def forward(self, input1, input2, input3):
-        output1 = self.forward_once(input1)
-        output2 = self.forward_once(input2)
-        output3 = self.forward_once(input3)
+        def forward(self, input1, input2, input3):
+            output1 = self.forward_once(input1)
+            output2 = self.forward_once(input2)
+            output3 = self.forward_once(input3)
 
-        return output1, output2, output3
+            return output1, output2, output3
 
-"""## Contrastive Loss / Triplet Loss"""
-
-
-
-class ContrastiveLoss(torch.nn.Module):
-    """
-    Contrastive loss function.
-    Based on: http://yann.lecun.com/exdb/publis/pdf/hadsell-chopra-lecun-06.pdf
-    """
-
-    def __init__(self, margin=2.0):
-        super(ContrastiveLoss, self).__init__()
-        self.margin = margin
-
-    def forward(self, output1, output2, label):
-        #begin = time()
-        euclidean_distance = F.pairwise_distance(output1, output2, keepdim = True)
-        loss_contrastive = torch.mean((1-label) * torch.pow(euclidean_distance, 2) +
-                                      (label) * torch.pow(torch.clamp(self.margin - euclidean_distance, min=0.0), 2))
-
-        #print('Time for contrastive Loss: ', time()-begin)
-        return loss_contrastive
-
-
-class TripletLoss(nn.Module):
-    """
-    Triplet loss
-    Takes embeddings of an anchor sample, a positive sample and a negative sample
-    """
-
-    def __init__(self, margin):
-        super(TripletLoss, self).__init__()
-        self.margin = margin
-
-    def forward(self, anchor, positive, negative, size_average=True):
-        #begin = time()
-        distance_positive = (anchor - positive).pow(2).sum(1)  # .pow(.5)
-        distance_negative = (anchor - negative).pow(2).sum(1)  # .pow(.5)
-        losses = F.relu(distance_positive - distance_negative + self.margin)
-
-        #print('Time for triplet loss: ', time()-begin)
-
-        return losses.mean() if size_average else losses.sum()
-
-"""## Training Time!"""
-print('Loading train dataloader. . .')
-train_dataloader = DataLoader(siamese_dataset,
-                        shuffle=True,
-                        num_workers=8,
-                        batch_size=Config.train_batch_size)
+    """## Contrastive Loss / Triplet Loss"""
 
 
 
+    class ContrastiveLoss(torch.nn.Module):
+        """
+        Contrastive loss function.
+        Based on: http://yann.lecun.com/exdb/publis/pdf/hadsell-chopra-lecun-06.pdf
+        """
 
-#net = SiameseNetwork_old().cuda()
-#net = SiameseNetwork_old()
-#net = SiameseNetwork().cuda()
+        def __init__(self, margin=2.0):
+            super(ContrastiveLoss, self).__init__()
+            self.margin = margin
 
+        def forward(self, output1, output2, label):
+            #begin = time()
+            euclidean_distance = F.pairwise_distance(output1, output2, keepdim = True)
+            loss_contrastive = torch.mean((1-label) * torch.pow(euclidean_distance, 2) +
+                                          (label) * torch.pow(torch.clamp(self.margin - euclidean_distance, min=0.0), 2))
 
-net = SiameseNetwork().cuda()
-
-
-net = nn.DataParallel(net,device_ids=[0,1,2,3])
-print('Model parallelized')
-'''
-print('Loading model. . .')
-loadPath = './res-resnet101-e245-b24.pth'
-'''
-#loadPath = './res-resnet101-e48-b24.pth'
-'''
-net.load_state_dict(torch.load(loadPath))
-print('\n\n\n\n\n Loaded model')
-'''
-#net = SiameseNetwork(Bottleneck, [3,4,23,3])
-#criterion = ContrastiveLoss()
-margin = 2.
-criterion = TripletLoss(margin)
-
-optimizer = optim.Adam(net.parameters(),lr = 0.0005 )
-
-# If we are loading instead
-#loadPath = './savedModels/yugioh-cropped-model.pth'
-#loadPath = './res-yugioh.pth'
-#loadPath = './savedModels/triplet-normalArch-thousandData-noSheer-batch64-0-res.pth'
-#loadPath = './savedModels/triplet-normalArch-thousandData-withSheer-batch16-0-res.pth'
+            #print('Time for contrastive Loss: ', time()-begin)
+            return loss_contrastive
 
 
-counter = []
-loss_history = [] 
-iteration_number= 0
+    class TripletLoss(nn.Module):
+        """
+        Triplet loss
+        Takes embeddings of an anchor sample, a positive sample and a negative sample
+        """
+
+        def __init__(self, margin):
+            super(TripletLoss, self).__init__()
+            self.margin = margin
+
+        def forward(self, anchor, positive, negative, size_average=True):
+            #begin = time()
+            distance_positive = (anchor - positive).pow(2).sum(1)  # .pow(.5)
+            distance_negative = (anchor - negative).pow(2).sum(1)  # .pow(.5)
+            losses = F.relu(distance_positive - distance_negative + self.margin)
+
+            #print('Time for triplet loss: ', time()-begin)
+
+            return losses.mean() if size_average else losses.sum()
+
+    """## Training Time!"""
+    print('Loading train dataloader. . .')
+    train_dataloader = DataLoader(siamese_dataset,
+                            shuffle=True,
+                            num_workers=8,
+                            batch_size=Config.train_batch_size)
+
+    net = SiameseNetwork().cuda()
 
 
-prevNum = -1
-for epoch in range(0,Config.train_number_epochs):
-    begin = time()
-    for i, data in enumerate(train_dataloader,0):
-        #print(i)
-        img_anc, img_pos, img_neg,_ = data
-        img_anc, img_pos, img_neg = img_anc.cuda(), img_pos.cuda(), img_neg.cuda()
-        optimizer.zero_grad()
-        output1,output2,output3 = net(img_anc, img_pos , img_neg)
-        loss_contrastive = criterion(output1,output2,output3)
-        loss_contrastive.backward()
-        optimizer.step()
-        # To prevent repetation of epoch
-        if i %10 == 0 and prevNum != epoch:
-            print("Epoch number {}\n Current loss {}\n".format(epoch,loss_contrastive.item()))
-            iteration_number +=10
-            counter.append(iteration_number)
-            loss_history.append(loss_contrastive.item())
-            prevNum = epoch
-    savePath = './res.pth'
+    net = nn.DataParallel(net,device_ids=[0,1,2,3])
+    print('Model parallelized')
+
+    margin = 2.
+    criterion = TripletLoss(margin)
+
+    optimizer = optim.Adam(net.parameters(),lr = 0.0005 )
+
+
+    counter = []
+    loss_history = [] 
+    iteration_number= 0
+    prevNum = -1
+
+    for epoch in range(0,Config.train_number_epochs):
+        begin = time()
+        for i, data in enumerate(train_dataloader,0):
+            #print(i)
+            img_anc, img_pos, img_neg,_ = data
+            img_anc, img_pos, img_neg = img_anc.cuda(), img_pos.cuda(), img_neg.cuda()
+            optimizer.zero_grad()
+            output1,output2,output3 = net(img_anc, img_pos , img_neg)
+            loss_contrastive = criterion(output1,output2,output3)
+            loss_contrastive.backward()
+            optimizer.step()
+            # To prevent repetation of epoch
+            if i %10 == 0 and prevNum != epoch:
+                print("Epoch number {}\n Current loss {}\n".format(epoch,loss_contrastive.item()))
+                iteration_number +=10
+                counter.append(iteration_number)
+                loss_history.append(loss_contrastive.item())
+                prevNum = epoch
+        savePath = './res.pth'
+        torch.save(net.state_dict(), savePath)
+        print(time()-begin, 's has passed')
+    savePath = './res-300-normalized.pth'
     torch.save(net.state_dict(), savePath)
-    print(time()-begin, 's has passed')
-#savePath = './resL-300-withRotate.pth'
-savePath = './res-300-normalized.pth'
-torch.save(net.state_dict(), savePath)
-
+ 
