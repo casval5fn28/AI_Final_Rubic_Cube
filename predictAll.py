79e23ace8a522a98e2abcdf8582928c8842e4656
Casval_dreamwalker <71252273+casval5fn28@users.noreply.github.com>
2022-06-12 23:09:31 +0800

    543&546 imagePath1 to imgPath1

--- predictAll.py
+++ predictAll.py
@@ -26,7 +26,7 @@ from time import time
 import random
 import torchvision.models as models
 import pickle
-
+from imgaug import augmenters as iaa
 
 useGPU = True
 
@@ -540,10 +540,10 @@ def compareNCards(imgPath0,imgPath1,targetDirName,n_compare=10):
 
     # N-way one shot learning evaluation
     # Compare positive images
-    euclidean_distance,featureMap = getSimilarRank(imagePath0,imagePath1)
+    euclidean_distance,featureMap = getSimilarRank(imgPath0,imgPath1)
     output1 = featureMap[0]
 
-    rankList.append((imagePath1.split('/')[-2],euclidean_distance.item(),imagePath1))
+    rankList.append((imgPath1.split('/')[-2],euclidean_distance.item(),imgPath1))
     featureMapList.append(output1)
 
 
